32c5146a2c064050274ed85efe3440e6c3d1cb03
Simple User <u316727@gen161.hs.shared.masterhost.ru>
2022-05-20 19:15:51 +0300

    virtual

---
+++ virtual.sonatel.ru/www/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2012-11-05 15:19:52 UTC
+#Software: Joomla Platform 11.4.0 Stable [ Brian Kernighan ] 03-Jan-2012 00:00 GMT
+
+#Fields: date	time	priority	clientip	category	message
+2012-11-05	15:19:52	INFO	93.182.0.115	Joomla FAILURE: 	Имя пользователя и пароль не совпадают или у вас еще нет учетной записи на сайте
